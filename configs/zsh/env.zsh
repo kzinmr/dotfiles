20dc5847e2a893a00e35765c692c48cbc0b0b6ea
Kazuki Inamura <kzinmr109@gmail.com>
2025-05-28 01:25:42 +0900

    add: git worktree commands

--- configs/zsh/env.zsh
+++ configs/zsh/env.zsh
@@ -16,4 +16,11 @@ alias g='git'
 alias s='git status'
 alias gdiff='git diff'
 alias gres='git restore'
-alias gswc='git switch'
+alias gswc='git switch -c'
+alias gswd='git switch -d'
+alias gpush='git push origin HEAD'
+alias ghpr='gh pr create --web'
+alias ggc='git add . && git commit -m.'
+alias ghead='git rev-parse HEAD'
+
+function gwtadd() { git worktree add "../$1" -b "$1" }
